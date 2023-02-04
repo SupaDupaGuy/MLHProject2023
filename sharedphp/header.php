8bb06a91e5f2e327ae014d74f1452915ce789625
Supa_Dupa_Guy <Markusje12@gmail.com>
2023-02-04 17:22:27 -0500

    Added the Header to be included on PHP Pages.

---
+++ sharedphp/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <link rel="stylesheet" href="../css/header.css">
+    </header>
+    <body>
+        <div class="containerStyle">
+            <div class="navContainer">
+                <a class="navButton" href="../login.php">
+                    <h3>Home</h3>
+                </a>
+                <a class="navButton" href="../login.php">
+                    <h3>Account</h3>
+                </a>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
